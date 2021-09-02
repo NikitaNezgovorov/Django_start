8a3aba9fa11ee6de2e8e115a50c471ea537d36cf
NikitaNezgovorov <75495473+NikitaNezgovorov@users.noreply.github.com>
2021-09-02 20:01:43 +0300

    HW3

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,27 +1,39 @@
 from django.shortcuts import render
+from .models import ProductCategory, Product
 
 
 # Create your views here.
 def main(request):
-    context = {
+    title = 'главная'
+
+    products = Product.objects.all()
+
+    content = {
         'page_title': 'главная',
+        'title': title,
+        'products': products
     }
-    return render(request, 'mainapp/index.html', context)
+    return render(request, 'mainapp/index.html', content)
 
 
-def products(request):
-    context = {
+def products(request, pk=None):
+    print(pk)
+
+    ProductCategorys = ProductCategory.objects.all()
+
+    content = {
         'page_title': 'каталог',
+        'ProductCategorys': ProductCategorys
+        ,
     }
-    return render(request, 'mainapp/products.html', context)
+    return render(request, 'mainapp/products.html', content)
 
 
 def contact(request):
-    context = {
+    content = {
         'page_title': 'контакты',
     }
-    return render(request, 'mainapp/contact.html', context)
+    return render(request, 'mainapp/contact.html', content)
 
 
-def temp(request):
-    return render(request, 'mainapp/temp1.html')
+
